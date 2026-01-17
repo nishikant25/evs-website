4fad5ee025249e2feefd45be42268bcd0b1e7016
nishikant25 <nishikantgavali5@gmail.com>
2026-01-17 12:03:29 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+function getAQI() {
+    const city = document.getElementById("cityInput").value;
 
+    if (city === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    fetch(`https://api.waqi.info/feed/${city}/?token=demo`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === "ok") {
+                document.getElementById("aqiResult").innerText =
+                    "AQI of " + city + " is: " + data.data.aqi;
+            } else {
+                document.getElementById("aqiResult").innerText =
+                    "AQI data not available";
+            }
+        });
+}
