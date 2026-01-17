975503be57180da262b6e9c0b49276f0aee36bc7
nishikant25 <nishikantgavali5@gmail.com>
2026-01-17 12:39:50 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function getAQI() {
     document.getElementById("aqiResult").innerText =
         "Fetching AQI for " + city + "...";
 
-    fetch(`https://api.waqi.info/feed/${city}/?token=demo`)
+    fetch(`https://api.waqi.info/feed/${city}/?token=d5bf0ab0a5163f50c07762966acdfc6542654ae0`)
         .then(res => res.json())
         .then(data => {
             if (data.status === "ok" && data.data.aqi !== "-") {
