5564afe65bafc8f8e324c25b36efc488ddd8e339
nishikant25 <nishikantgavali5@gmail.com>
2026-01-17 12:56:01 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,14 +12,17 @@ function getAQI() {
     fetch(`https://api.waqi.info/feed/${city}/?token=d5bf0ab0a5163f50c07762966acdfc6542654ae0`)
         .then(res => res.json())
         .then(data => {
-            if (data.status === "ok") {
-                document.getElementById("aqiResult").innerText =
-                    `AQI of ${city}: ${data.data.aqi}`;
-            } else {
-                document.getElementById("aqiResult").innerText =
-                    "AQI data not available for this city.";
-            }
-        })
+    if (data.status === "ok") {
+        const station = data.data.city.name;
+        const time = data.data.time.s;
+
+        document.getElementById("aqiResult").innerText =
+            `AQI: ${data.data.aqi}
+             Station: ${station}
+             Updated at: ${time}`;
+    }
+})
+
         .catch(error => {
             document.getElementById("aqiResult").innerText =
                 "Error fetching AQI data.";
